a28b13277241887475f24538af35195357645bc0
Alfapsycho <psao69@gmail.com>
2019-02-03 20:48:36 +0100

    Fixed speed Test

--- tests/CarTest.java
+++ tests/CarTest.java
@@ -12,5 +12,21 @@ public class CarTest {
         assertEquals(2, testSaab.getNrDoors());
     }
 
+    public void maxSpeedVolvo(){
+        Car testVolvo = new Volvo240();
+        for(int i =0; i<= testVolvo.getEnginePower(); i++){
+            testVolvo.gas(1);
+        }
+        assertEquals(testVolvo.getCurrentSpeed(), testVolvo.getEnginePower(), 0);
+    }
+
+    public void maxSpeedSaab(){
+        Car testSaab = new Saab95();
+        for (int i = 0; i < testSaab.getEnginePower(); i++) {
+            testSaab.gas(1);
+        }
+        assertEquals(testSaab.enginePower, testSaab.getEnginePower(), 0);
+    }
+
 
 }
